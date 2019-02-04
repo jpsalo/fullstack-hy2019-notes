b60e0abfbb9787ce492b2b5344f735324e1256e5
JP Salo <jpsalo@zoho.com>
2019-02-04 18:52:49 +0200

    Prefix backend url by /api

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ let notes = [
   },
 ]
 
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
@@ -41,7 +41,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/notes', (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -62,7 +62,7 @@ app.post('/notes', (request, response) => {
   response.json(note)
 })
 
-app.get('/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
   if (note) {
@@ -72,7 +72,7 @@ app.get('/notes/:id', (request, response) => {
   }
 })
 
-app.delete('/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
