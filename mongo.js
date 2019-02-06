9635e9b79d05e56b9cd3a63bc4de86063e392386
JP Salo <jpsalo@zoho.com>
2019-02-06 13:21:11 +0200

    MongoDB

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb://fullstack:${password}@ds223605.mlab.com:23605/fullstack2019-notes`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'asd',
+//   date: new Date(),
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+Note.find({ important: true }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
